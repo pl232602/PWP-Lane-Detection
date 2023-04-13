4316f9d675aaab6727f495e3964211b284d25be6
Soulja_boi <pl232602@ahschool.com>
2023-04-12 22:52:12 -0400

    restructured files

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from flask import Flask
-from web_app.ui import root
+from ui import root
 
-app = Flask(__name__,template_folder="web_app/templates")
+app = Flask(__name__)
 app.register_blueprint(root)
-app.run(host="0.0.0.0")
