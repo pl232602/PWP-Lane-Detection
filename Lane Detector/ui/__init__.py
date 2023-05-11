e3a3389ad030193d87e20dbcc9e18409ddc2700b
Niles Alexis <90462248+pl232602@users.noreply.github.com>
2023-05-11 06:45:36 -0400

    Update __init__.py
    Added documentation

--- Lane Detector/ui/__init__.py
+++ Lane Detector/ui/__init__.py
@@ -6,16 +6,16 @@ from loginapi import user
 
 root= flask.Blueprint("root",__name__)
 
-@root.route("/",methods=["GET","POST"])
+@root.route("/",methods=["GET","POST"]) #login page
 def submit():
     if flask.request.method=="POST":
         username=request.form["username"] #Take user input from webpage.
         password=request.form["password"]
-        checkuser=user([request.form["username"],request.form["password"]])
+        checkuser=user([request.form["username"],request.form["password"]]) #check if user is in the database
         if checkuser.check_user() == True:
-            return render_template("index.html")
+            return render_template("index.html") #if the user is in database, display main page
         else:
-            print("bad tingz")
+            print("login failed") #if user is not in database, do not show main page
 
 
         #Inputted usname and password combination will be hashed here, and the logapi file userapi file will be used to check if a hashed copy exists in the user database.
@@ -25,17 +25,17 @@ def submit():
 @root.route("/create_user",methods=['GET','POST'])
 def create_user():
     if request.method=="POST":
-        adduser=user([request.form["username"],request.form["password"]])
+        adduser=user([request.form["username"],request.form["password"]]) #get input from user
         print([request.form["username"],request.form["password"]])
-        adduser.credential_hash()
+        adduser.credential_hash() #hash credentials using cryptographically secure methods
         if adduser.add_user() == False:
-            return render_template("user_exists.html")
+            return render_template("user_exists.html") #if user with matching username exists, notify user
         else:
-            adduser.add_user()
+            adduser.add_user() #ifstching username doesnt exist, notify user of successful credential creation 
             return render_template("user_created.html")
     return render_template("create_user.html")
 
 @root.route("/user_exists",methods=["GET","POST"])
 def user_exists():
     if request.method=="POST":
-        return render_template("create_user.html")
+        return render_template("create_user.html") #return user to user creation 
