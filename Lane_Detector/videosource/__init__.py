0a73ff7cd5423cf8cdd04bc624ae3d269511d55e
Niles Alexis <pl232602@ahschool.com>
2023-05-11 08:36:54 -0400

    renamed source folder

--- Lane_Detector/videosource/__init__.py
+++ Lane_Detector/videosource/__init__.py
@@ -1,8 +1,7 @@
 import flask
 from flask import Response
-import os
 
-from camera import gen_localhttp_res
+from Lane_Detector.videosource.camera import gen_localhttp_res
 
 camera_blueprint = flask.Blueprint('camera_blueprint', __name__, url_prefix='/camera') #creates flask blueprint for camera components to be registered with flask app
 
