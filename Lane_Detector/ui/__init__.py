e946b563915efa85859c74323c7c4551e7746d23
Niles Alexis <pl232602@ahschool.com>
2023-05-28 17:00:45 -0400

    more ui improvements

--- Lane_Detector/ui/__init__.py
+++ Lane_Detector/ui/__init__.py
@@ -6,14 +6,17 @@ from loginapi import user
 
 root= flask.Blueprint("root",__name__)
 
+current_user = "null"
+
 @root.route("/",methods=["GET","POST"]) #login page
 def submit():
     if flask.request.method=="POST":
         username=request.form["username"] #Take user input from webpage.
+        current_user = username
         password=request.form["password"]
         checkuser=user([request.form["username"],request.form["password"]]) #check if user is in the database
         if checkuser.check_user() == True:
-            return render_template("index.html") #if the user is in database, display main page
+            return render_template("index.html", session_user = current_user) #if the user is in database, display main page
         else:
             print("login failed") #if user is not in database, do not show main page
 
